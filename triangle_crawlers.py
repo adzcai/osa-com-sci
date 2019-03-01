f9a253b165f0c7f173880c410c224a96ffbace79
Alexander Cai <alexandercai@outlook.com>
2019-02-28 19:28:45 -0700

    working triangle crawlers

--- triangle_crawlers.py
+++ triangle_crawlers.py
@@ -1,20 +1,20 @@
 # Assignment 2 - Triangle Crawlers
 # Author: Alexander Cai
 
+# Importing used libraries
 import pygame, sys
 from pygame.locals import *
 from math import sin, cos, radians
 from random import choice
 
+# Constant variables to store the state of our program
 WIDTH = 480
 HEIGHT = 480
 FPS = 30
-WAIT_TIME = 500
+WAIT_TIME = 500 # The number of milliseconds between moves
 NUM_CRAWLERS = 100000
 LETTERS = ("A", "B", "C", "D")
-at_positions = {letter: (NUM_CRAWLERS if letter == "A" else 0) for letter in LETTERS }
-
-font_name = pygame.font.match_font('arial')
+at_positions = {letter: (NUM_CRAWLERS if letter == "A" else 0) for letter in LETTERS} # Stores the number of crawlers at each position
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -42,60 +42,45 @@ class Crawler(pygame.sprite.Sprite):
 		at_positions[self.pos] += 1
 		at_positions[self.prev_pos] -= 1
 
-class Year(pygame.sprite.Sprite):
+class Day(pygame.sprite.Sprite):
+	"""A sprite used to store the current dar"""
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		self.font = pygame.font.Font(None, WIDTH // 24)
 		self.font.set_italic(1)
-		self.color = BLACK
 		self.value = 0
 		self.update()
-		self.rect = self.image.get_rect().move(10, 450)
+		self.rect = self.image.get_rect()
+		self.rect.bottomleft = (10, HEIGHT - 10)
 		
 	def update(self):
 		self.value += 1
-		msg = f"Year: {self.value}"
-		self.image = self.font.render(msg, 0, self.color)
+		msg = f"Day: {self.value}"
+		self.image = self.font.render(msg, 0, BLACK)
 
 class Circle(pygame.sprite.Sprite):
+	"""A class to display a possible position for a crawler."""
 	def __init__(self, x, y, r, letter):
 		pygame.sprite.Sprite.__init__(self)
 		self.rect = pygame.Rect(x - r, y - r, r * 2, r * 2)
 		self.image = pygame.Surface((self.rect.width, self.rect.height))
 		self.circle_rect = pygame.Rect(0, 0, self.rect.width, self.rect.height)
-		self.image.fill(WHITE)
 		self.letter = letter
 
 	def update(self):
+		"""Updates the image to match the number of crawlers at this circle."""
+		self.image.fill(WHITE)
 		pygame.draw.ellipse(self.image, BLACK, self.circle_rect, 3)
 		draw_text(self.image, self.letter, BLACK, 24, self.rect.width // 2, self.rect.height // 4)
 		draw_text(self.image, str(at_positions[self.letter]), BLACK, 20, self.rect.width // 2, self.rect.height // 2)
 
 def draw_text(surf, text, color, size, x, y):
 	"""Draws text to :surf: with a given font size."""
-	font = pygame.font.Font(font_name, size)
+	font = pygame.font.Font(None, size) # Gets the default pygame font, which is fine for our purposes
 	text_surface = font.render(text, True, color)
 	text_rect = text_surface.get_rect()
 	text_rect.midtop = (x, y)
 	surf.blit(text_surface, text_rect)
-
-def draw_arrow(p1=(0, 0), p2=(0, 0), direction="left"):
-	"""Draws an arrow (line with a tick at the end) from p1 to p2 with the tick facing *direction*."""
-	v = pygame.math.Vector2(p2[0] - p1[0], p2[1] - p1[1])
-	ahlen = v.length() // 24
-	ahhor = ahlen * 2 / 3
-	v.normalize_ip()
-
-	perp = pygame.math.Vector2(-v.y, v.x)
-
-	lastp = []
-
-	if direction == "left":
-		lastp = (p2[0] - ahlen * v.x + ahhor * perp.x, p2[1] - ahlen * v.y + ahhor * perp.y)
-	else:
-		lastp = (p2[0] - ahlen * v.x - ahhor * perp.x, p2[1] - ahlen * v.y - ahhor * perp.y)
-
-	pygame.draw.lines(screen, BLACK, False, (p1, p2, lastp))
 
 def show_start_screen():	
 	screen.fill(WHITE)
@@ -117,12 +102,36 @@ def show_start_screen():
 		clock.tick(FPS)
 
 def show_results(result, total_lifetime):
-	colmaxes = {i: max(len(str(x)) for x in (row[i] for row in result)) for i in range(3)} # Use a big old dict comprehension to nicely pack up the values
-	for row in result:
-		print(' '.join(str(row[i]).rjust(colmaxes[i]) for i in range(3)))
-		
-	print("The average lifetime was " + str(total_lifetime / NUM_CRAWLERS))
+	screen.fill(WHITE)
+	
+	font_size = int((HEIGHT * 3 / 4) // len(result))
+	start_height = HEIGHT // 8
 
+	draw_text(screen, "The average lifetime was " + str(total_lifetime / NUM_CRAWLERS), BLACK, 24, WIDTH // 2, 10)
+
+	for i in range(len(result)): # For each row of the results
+		for j in range(3): # Each of the three columns
+			draw_text(screen, str(result[i][j]), BLACK, font_size, (j+1) * WIDTH // 4, start_height + i * font_size)
+	
+	draw_text(screen, "Press enter to simulate again or press a key to end", BLACK, 24, WIDTH // 2, HEIGHT - 24 - 10)
+
+	pygame.display.flip()
+
+	waiting = True
+	while waiting:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYUP:
+				if event.key == K_RETURN:
+					waiting = False
+				else:
+					pygame.quit()
+					sys.exit()
+
+		clock.tick(FPS)
+		
 def main():
 	global screen, clock, crawlers
 
@@ -135,13 +144,13 @@ def main():
 	font = pygame.font.match_font("arial")
 		
 	crawlers = pygame.sprite.Group(Crawler() for i in range(NUM_CRAWLERS))
-	year = pygame.sprite.GroupSingle(Year())
+	day = pygame.sprite.GroupSingle(Day())
 	circles = pygame.sprite.Group()
 	
 	r = min(WIDTH, HEIGHT) // 3
 	circler = r // 3
 	for i in range(3):
-		theta = radians(120 * i)
+		theta = radians(120 * i - 90) # - 90 makes the top one vertically above center
 		x = int(WIDTH / 2 + cos(theta) * r)
 		y = int(HEIGHT / 2 + sin(theta) * r)
 		circles.add(Circle(x, y, circler, LETTERS[i]))
@@ -159,23 +168,26 @@ def main():
 				pygame.quit()
 				sys.exit()
 
-		if pygame.time.get_ticks() - last_move > WAIT_TIME: # We move onto the next year
+		if pygame.time.get_ticks() - last_move > WAIT_TIME: # We move onto the next Day
 			crawlers.update()
-			year.update()
+			day.update()
+			circles.update()
+
 			numcrawlers = len(crawlers)
 			total += numcrawlers
-			result.append((year.sprite.value, numcrawlers, total))
+			result.append((day.sprite.value - 1, numcrawlers, total)) # Should be before the Day updates
 
 			if numcrawlers == 0:
-				show_results(result, total)
-				pygame.quit()
-				sys.exit()
-			last_move = pygame.time.get_ticks()
+				show_results(result, total) # This shows the results and ends the program when the user clicks
+				global at_positions
+				at_positions = {letter: (NUM_CRAWLERS if letter == "A" else 0) for letter in LETTERS } # Reset the crawlers at each point
+				main() # Restart the program
+				
+			last_move = pygame.time.get_ticks() # Set the last move to now
 
-		circles.update()
-
+		# Drawing and rendering
 		screen.fill(WHITE)
-		year.draw(screen)
+		day.draw(screen)
 		circles.draw(screen)
 
 		pygame.display.flip()
