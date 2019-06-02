3f857c4066d23162a9263ae294bd628393e3f9dd
Alexander Cai <alexandercai@outlook.com>
2019-06-02 16:32:57 -0600

    cleaned up battleship

--- battleship.py
+++ battleship.py
@@ -1,22 +1,7 @@
 # Assignment 3 - Battleship
-
-# Battleship is a war-themed board game for two players in which the opponents try to guess the location of the other's various ships. A paper and pencil version of the game dates back to World War I, but most people are familiar with the game through the plastic board game that was first marketed by the Milton Bradley Company in 1967. Since then, the game has spawned various video games and smartphone app variations. Today, the board game version is produced by Hasbro, which acquired Milton Bradley in 1984. 
- 
-# The object of the game is to guess the location of the ships each player hides on a plastic grid containing vertical and horizontal space coordinates. Players take turns calling out row and column coordinates on the other player's grid in an attempt to identify a square that contains a ship. 
- 
-# The game board each player gets has two grids. One of the grids is used by the player to "hide" the location of his own ships, while the other grid is used to record the shots fired toward the opponent and to document whether those shots were hits or misses. The goal of the game is to sink all of the opponent's ships by correctly guessing their location on the grid. 
-
-# Create a program for the user to play against the computer. Instead of each player having ships and their own grid, only the computer does. The computer will strategically place three ships that are 3 squares each on the grid and the user will try to guess their location in the minimum amount of guesses. For this game you must use a two dimensional array (7 x 7).  Randomly assign the placement of the ships to the grid. Once the ships are in place, the user should try to guess the location of each ship.  The game should keep track of the amount of guesses from the user. 
-# Add creativity to this game and go beyond the assignment requirements. Also try to be as efficient as possible with your code.
-
-# When the project is complete please submit a folder with all of your files. Present your program to your teacher and answer questions about the code and overall program. 
-
-# Marks will be awarded based on the following:
-
-# Project Requirements and Understanding / 30 marks
-# Planning and Coding / 30 marks
-# Program Design / 20 marks
-# Creativity and Extension / 20 marks
+# By Alexander Cai
+# For the Javascript version of my code with better graphics, see
+# https://github.com/piguyinthesky/react-tleship
 
 from random import randint
 from collections import namedtuple
@@ -61,8 +46,6 @@ class Grid:
       print(f' {i + 1} | ' + ' | '.join(map(str, row)))
 
   def guess(self, r, c):
-    print(r)
-    print(c)
     if self.grid[r][c] == EMPTY:
       self.place(r, c, 'O')
       self.display()
